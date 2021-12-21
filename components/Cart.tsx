685e72620cca6c9b856de07bc0df5bf915c930f5
Rozaviht <miguel.vihto@rozaviht.com>
2021-12-21 12:48:35 +0100

    Added styles to empty cart, and changed some styles for cart slider

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,21 +1,30 @@
 import { CartProps } from '@components/Navbar'
+import Image from 'next/image'
+
+import emptyBasket from '@img/empty-basket.svg'
 
 
-const Basket = ({handleShowCart, showCart}: CartProps) => {
+const Cart = ({handleShowCart, showCart}: CartProps) => {
 
 
   return (
-    <div className={showCart ? "cart-slide dropped" : "cart-slide"}>
+    <div className={showCart ? "cart dropped" : "cart"}>
       <button className="close-bt" onClick={handleShowCart}>
         <div className="line-left"></div>
         <div className="line-right"></div>
       </button>
       <h2>Tu cesta de la compra</h2>
-      <div className="empty-cart">
-        <h3>!Vaya¡ Tu cesta de la compra esta vacía</h3>
+      <div className="containerflx--column">
+        <div className="empty-cart">
+          <div className="empty-cart-text">
+            <h3>!Vaya¡</h3>
+            <h3>Tu cesta de la compra esta vacía</h3>
+          </div>
+          <Image src={emptyBasket} width={200} height={200}/>
+        </div>
       </div>
     </div>
   )
 }
   
-export default Basket
+export default Cart
