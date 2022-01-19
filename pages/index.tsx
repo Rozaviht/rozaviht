73906319f542423e2d34e10225da233a36fe3b0a
Rozaviht <miguel.vihto@rozaviht.com>
2022-01-19 14:34:28 +0100

    Added images and video for indexpage, and changed some css for the layout in the sections.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,20 +4,26 @@ import Image from 'next/image'
 import Logo from '@img/Logo.svg'
 import oilIllustration from '@img/oil-ilustration.svg'
 import oilSectionBanner from '@img/pexels-cottonbro-seccion-aceite.jpg'
-import rozadaySectionBanner from '@img/pexels-cottonbro-rozaday-section.jpg'
-import rozavihtSectionBanner from '@img/pexels-ganta-guywatching.jpg'
+import rozadaySectionBanner from '@img/Image-sectionrozaday.jpg'
+import rozavihtSectionBanner from '@img/Image-sectionrozaviht.jpeg'
 
 const index = () => {
   return (
     <div className="home-wrapper">
-      <div className="home-banner"></div>
+      <div className="home-banner">
+        <video className="video-banner" autoPlay loop playsInline preload='metadata'>
+          <source type='video/mp4' src="/rozaviht-banner-video.mp4" />
+        </video>
+      </div>
       <div className="home-info">
         <h3 className="font-Lora">Siempre a tu disposición</h3>
         <p className="font-Lora">Envios solo a Península</p>
       </div>
       <div className="section section--oil">
         <div className="section-banner">
-          <Image className="section-banner-img" src={oilSectionBanner} alt=""  width={1000} height={1200}/>
+          <div className="image-container">
+            <Image src={oilSectionBanner} alt=""  width={150} height={100} layout="responsive"/>
+          </div>
           <h1 className="section-banner-title">ACEITE DE CBD</h1>
         </div>
         <div className="container--flex">
@@ -28,7 +34,9 @@ const index = () => {
             en otros lugares de la Unión Europea y en Estados Únidos esta permitido su uso alimenticio,
             donde comúnmente se usa de manera sublingual.
           </p>
-          <Image className="oil-ilustration" src={oilIllustration} alt=""  width={200} height={400}/>
+          <div className="image-container">
+            <Image src={oilIllustration} alt=""  width={270} height={340} layout="responsive"/>
+          </div>
           <p>
             Esto se debe a que el aceite de CBD, ha 
             demostrado en diferentes estudios ayudar
@@ -48,7 +56,9 @@ const index = () => {
       </div>
       <div className="section section--rozaday">
         <div className="section-banner">
-          <Image className="section-banner-img" src={rozadaySectionBanner} alt=""  width={1000} height={900}/>
+          <div className="image-container">
+            <Image src={rozadaySectionBanner} alt=""  width={150} height={100} layout="responsive"/>
+          </div>
           <h1 className="section-banner-title" >Rodazay</h1>
         </div>
         <div className="container--flex">
@@ -76,7 +86,9 @@ const index = () => {
         <button className="cta--secdry"><Link href="/aceite-cbd"><a>Ir a rozaday</a></Link></button>
       </div>
       <div className="section section--rozaviht">
-        <Image className="logo-rozaviht-section" src={Logo} alt=""  width={200} height={80}/>
+        <div className="image-container">
+          <Image src={Logo} alt="" width={340} height={270} layout="responsive"/>
+        </div>
         <p>
           Los principales beneficios del aceite de CBD son la anti inflamación y la relajación,
           con un par de gotas en la zona notaras los efectos casi inmediatos. Por otro lado,
@@ -85,7 +97,9 @@ const index = () => {
           donde comúnmente se usa de manera sublingual.
         </p>
         <button className="cta--secdry"><Link href="/aceite-cbd"><a>Conocenos</a></Link></button>
-        <Image className="img-rozaviht-section" src={rozavihtSectionBanner} alt=""  width={1000} height={1200}/>
+        <div className="image-container">
+          <Image src={rozavihtSectionBanner} alt="" width={150} height={100} layout='responsive'/>
+        </div>
       </div>
     </div>
   )
