4a4871ad6b707ba1e7b2d239fab4dbc777b59c74
Rozaviht <miguel.vihto@rozaviht.com>
2022-04-27 17:48:22 +0200

    Ending the qr Page. First Attempt.

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
-export const reactStrictMode = true
-export const images = {
-  domains: ["rozaviht-media.s3.eu-west-3.amazonaws.com"]
+module.exports =  {
+      reactStrictMode: true,
+      images: {
+        domains: ["rozaviht-media.s3.eu-west-3.amazonaws.com"]
+      },
+      webpack(config) {
+        const fileLoaderRule = config.module.rules.find(
+          (rule) => rule.test && rule.test.test('.svg')
+        )
+        fileLoaderRule.exclude = /\.svg$/
+        config.module.rules.push({
+          test: /\.svg$/,
+          loader: require.resolve('@svgr/webpack')
+        })
+        return config
+      }
+      
 }
-export function webpack(config) {
-  const fileLoaderRule = config.module.rules.find(
-    (rule) => rule.test && rule.test.test('.svg')
-  )
-  fileLoaderRule.exclude = /\.svg$/
-  config.module.rules.push({
-    test: /\.svg$/,
-    loader: require.resolve('@svgr/webpack')
-  })
-  return config
-}
-  
-
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,10 +24,13 @@ type AppPropsWithLayout = AppProps & {
     <>
       <Head>
           <link rel="stylesheet" href="fonts/style.css" />
-          <link rel="shortcut icon" type='image/ico' href="img/favicon.ico" />
+          <link rel="shortcut icon" sizes='224x256' type='image/ico' href="img/favicon.ico" />
           <title>Rozaviht: Te cuidas, Te cuidamos y Lo cuidamos</title>
           <meta name='description' content='En Rozaviht nuestro objetivo es proveer de productos a las personas que cuiden de ellas. Siempre sin dejar de lado el cuido medioambiental que nos define también como marca.' />
-          <meta name='viewport' content='width=device-width,initial-scale=1.0' />
+          <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1' />
+          <meta name="apple-mobile-web-app-title" content="Rozaviht: Te cuidas, Te cuidamos y Lo cuidamos"/>
+          <meta name="format-detection" content="telephone=no"/>
+          <meta name="format-detection" content="address=no"/>   
       </Head>
       <AppProvider>
         {getLayout(<Component {...pageProps} />)}
