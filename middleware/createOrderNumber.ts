b55372931f88db671dd309a39722934287495c8d
Rozaviht <miguel.vihto@rozaviht.com>
2022-05-21 22:19:22 +0200

    Fixed order number if

--- middleware/createOrderNumber.ts
+++ middleware/createOrderNumber.ts
@@ -14,13 +14,13 @@ export const createOrderNumber: FieldResolver<
     })
     /* 456789, 45879, 789456 this numbers was teste in redsys before so redsys wont enable to do the transaction with theses numbers.*/
 
-    if (oderNumbersInDb.length === 456789 || oderNumbersInDb.length === 45879 || oderNumbersInDb.length === 789456) {
+/*     if (oderNumbersInDb.length === 456789 || oderNumbersInDb.length === 45879 || oderNumbersInDb.length === 789456) {
       await prisma.order_number.create({
         data: {
           number: oderNumbersInDb.length + 2
         }
       })
-    }
+    } */
 
     await prisma.order_number.create({
       data: {
