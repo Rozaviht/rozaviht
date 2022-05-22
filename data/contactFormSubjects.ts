8126409d1ec06405e26b07c07ec79e3142f1e445
Rozaviht <miguel.vihto@rozaviht.com>
2022-05-22 19:57:14 +0200

    Merge develop 4

---
+++ data/contactFormSubjects.ts
@@ -0,0 +1,33 @@
+export const subjects = [
+  {
+    "category": "ENTREGA",
+    "subjects": [
+      "Problema con la entrega de mi producto"
+    ]
+  },
+  {
+    "category": "PRODUCTO",
+    "subjects": [
+      "Problema con  mi producto",
+      "Devolución de mi producto",
+      "Cancelación de mi producto"
+    ]
+  },
+  {
+    "category": "PAGO",
+    "subjects": [
+      "Incidente con mi pago en línea"
+    ]
+  },
+  {
+    "category": "INFORMACIÓN",
+    "subjects": [
+      "Información sobre nosotros",
+      "Información sobre algún producto"
+    ]
+  },
+  {
+    "category": "OTRO",
+    "subjects": ["Otro"]
+  }
+]
