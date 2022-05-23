9d7e5b5d24ed9957a0d84d16f7122509979cf245
Rozaviht <miguel.vihto@rozaviht.com>
2022-05-23 23:14:17 +0200

    Fixed key map in faqcard

--- components/FAQCard.tsx
+++ components/FAQCard.tsx
@@ -12,7 +12,7 @@ export default function FAQCard ({faqCategory}:faqCardProps) {
       <div className="faqCard__icon"><img src={faqCategory.icon} alt="" /></div>
       <h2>{faqCategory.title}</h2>
       {faqCategory.themeCuestions.slice(0 , 2).map( (cuestion, index) => 
-        < Link href={`/preguntas-frecuentes/${faqCategory.title.toLowerCase()}`}>
+        < Link href={`/preguntas-frecuentes/${faqCategory.title.toLowerCase()}`} key={index}>
           <a>{cuestion.cuestion}</a>
         </Link>
       )}
